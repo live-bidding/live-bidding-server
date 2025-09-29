7df47c28a8b1910131c74e8177be52c02d55fde0
권장순 <jsoonworld@gmail.com>
2025-09-29 22:07:20 +0900

    feat(project): init

---
+++ src/main/java/com/livebidding/LiveBiddingServerApplication.java
@@ -0,0 +1,13 @@
+package com.livebidding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiveBiddingServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiveBiddingServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/livebidding/LiveBiddingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.livebidding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiveBiddingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
